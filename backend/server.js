a142793bd80217957740c469b25c0978cea15f70
Morris <morrisho215215@gmail.com>
2024-04-13 16:46:31 +0800

    Connecting Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path')
+const colors = require('colors');
 const dotenv = require('dotenv').config();
+const { connectDB } = require('./config/db'); 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
 
+app.use('/api/recipes', require('./routes/recipeRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+// app.use('/api/reviews', require('routes/reviewRoutes'))
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
