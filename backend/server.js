11f155479f5b504868cc50bed0c61aa9fa24ba87
Morris <morrisho215215@gmail.com>
2024-04-16 16:49:03 +0800

    Add error middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { connectDB } = require('./config/db'); 
+const { errorHandler } = require('./middleware/errormiddleware');
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
 
+app.use(errorHandler)
 app.use('/api/recipes', require('./routes/recipeRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/reviews', require('./routes/reviewRoutes'));
