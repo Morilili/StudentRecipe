875dd6b35d1cfa33532546e64c2355c52b52b0ff
Morris <morrisho215215@gmail.com>
2024-04-17 14:18:15 +0800

    Add authentication

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -8,10 +8,11 @@ const recipeRouter = express.Router()
 // Load middleware
 const { adminprotect }= require("../middleware/auth")
 
+
 // Load controllers
 const { getAllRecipes, getSingleRecipe, createNewRecipe, updateRecipe, deleteRecipe} = require("../controllers/recipe")
 
-recipeRouter.route('/').get(getAllRecipes).post( upload.any('image'), adminprotect, createNewRecipe)
+recipeRouter.route('/').get(getAllRecipes).post(upload.any('image'), adminprotect, createNewRecipe)
 recipeRouter.route('/:recipe_id').get(getSingleRecipe).put(adminprotect, upload.any('image'), updateRecipe).delete(adminprotect, deleteRecipe)
 
 module.exports = recipeRouter
