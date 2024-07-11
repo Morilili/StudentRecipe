3669f07c69ffedc5ca1f70cd0b1fb82431486385
Morris <morrisho215215@gmail.com>
2024-07-11 12:28:17 +0800

    Create pages

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -117,7 +117,7 @@ function Register() {
             
           </div>
           <div className='form-group'>
-            <button type='submit' className='btn btn-block'>
+            <button type='submit' className='.btn btn-block'>
               Submit
             </button>
           </div>
