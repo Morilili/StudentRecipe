eab696e21361ea71b6f0e78ab59f65affd843221
Morris <morrisho215215@gmail.com>
2024-08-16 11:43:37 +0800

    Containerize application using Docker

--- backend/index.js
+++ backend/index.js
@@ -24,12 +24,12 @@ app.use('/api/recipes', require('./routes/recipeRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/reviews', require('./routes/reviewRoutes'));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
+//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+// } else {
+//   app.get('/', (req, res) => res.send('Please set to production'));
+// }
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
